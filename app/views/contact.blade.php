778d3214861afb2a4978065b287e54de777e5123
Jbns89 <Jbns89@gmail.com>
2014-12-01 17:08:00 -0600

    no message

---
+++ app/views/contact.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+
+@section('header')
+<style>
+    .navbar-default .navbar-nav>li>a 
+    {
+        color: #000;
+    }
+
+    .navbar-default .navbar-brand {
+        color: #000;
+    }
+</style>
+<title>Contact Us</title>
+@stop
+
+@extends('layouts.master')
+
+@section('header')
+    <title>Contact Us</title>
+@stop
+
+@section('content')
+    <h1>Contact Us</h1>
+@stop
